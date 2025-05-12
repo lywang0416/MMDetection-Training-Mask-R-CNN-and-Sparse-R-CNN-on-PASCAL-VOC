9c9e2e083becebc4f5273c19db4923126d077a4f
王乐阳 <wangleyang@wangyueyangdeMacBook-Air.local>
2025-05-12 23:29:23 +0800

    update second edition

--- mmdetection/configs/mask_rcnn/mask-rcnn_r50_fpn_1x_voc.py
+++ mmdetection/configs/mask_rcnn/mask-rcnn_r50_fpn_1x_voc.py
@@ -4,40 +4,40 @@ _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
-# 1. 数据集类型
-dataset_type = 'VOCDataset'
-data_root = 'data/VOCdevkit/'
+# # 1. 数据集类型
+# dataset_type = 'VOCDataset'
+# data_root = 'data/VOCdevkit/'
 
-# 2. 类别名（VOC有20类）
-classes = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
-           'bus', 'car', 'cat', 'chair', 'cow',
-           'diningtable', 'dog', 'horse', 'motorbike', 'person',
-           'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
+# # 2. 类别名（VOC有20类）
+# classes = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+#            'bus', 'car', 'cat', 'chair', 'cow',
+#            'diningtable', 'dog', 'horse', 'motorbike', 'person',
+#            'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
 
-# 3. 数据集配置
+# # 3. 数据集配置
 
-data = dict(
-    train=dict(
-        type=dataset_type,
-        ann_file=data_root + 'VOC2007/ImageSets/Main/trainval.txt',
-        img_prefix=data_root + 'VOC2007/',
-        classes=classes),
-    val=dict(
-        type=dataset_type,
-        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
-        img_prefix=data_root + 'VOC2007/',
-        classes=classes),
-    test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
-        img_prefix=data_root + 'VOC2007/',
-        classes=classes)
-)
+# data = dict(
+#     train=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'VOC2007/ImageSets/Main/trainval.txt',
+#         img_prefix=data_root + 'VOC2007/',
+#         classes=classes),
+#     val=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+#         img_prefix=data_root + 'VOC2007/',
+#         classes=classes),
+#     test=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+#         img_prefix=data_root + 'VOC2007/',
+#         classes=classes)
+# )
 
-# 4. 其他训练设置
-model = dict(
-    roi_head=dict(
-        bbox_head=dict(num_classes=20),
-        mask_head=dict(num_classes=20)
-    )
-)
+# # 4. 其他训练设置
+# model = dict(
+#     roi_head=dict(
+#         bbox_head=dict(num_classes=20),
+#         mask_head=dict(num_classes=20)
+#     )
+# )
